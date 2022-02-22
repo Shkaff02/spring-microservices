55882d3506e038cd5338f61ab438fc6d5d47c630
Shkaff02 <78539412+Shkaff02@users.noreply.github.com>
2022-02-22 22:18:37 +0200

    - Added Notification module

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,6 +2,8 @@ package com.amigoscode.customer;
 
 import com.amigoscode.clients.fraud.FraudCheckResponse;
 import com.amigoscode.clients.fraud.FraudClient;
+import com.amigoscode.clients.notification.NotificationClient;
+import com.amigoscode.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -23,10 +25,18 @@ public class CustomerService {
         // todo: check if email not taken
         customerRepository.saveAndFlush(customer);
 
-        FraudCheckResponse fraudCheckResponse =  fraudClient.isFraudster(customer.getId());
+        FraudCheckResponse fraudCheckResponse=  fraudClient.isFraudster(customer.getId());
        if (fraudCheckResponse.isFraudster()) {
            throw new IllegalStateException("fraudster");
        }
+
+       notificationClient.sendNotification(
+               new NotificationRequest(
+                       customer.getId(),
+                       customer.getEmail(),
+                       String.format("Hi %s, welcome to Amigoscode...", customer.getFirstName())
+               )
+       );
         // todo: send notification
     }
 }
